338f8eb673708c4841eb595c2104df6de0500dc3
ibdkn <ibudiakina@gmail.com>
2023-12-04 23:22:55 +0300

    changed 05.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -14,7 +14,7 @@ switch (true) {
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break;
-    default:
+    case rule.length === 25:
         console.log("50 на 50");
         break;
 }
