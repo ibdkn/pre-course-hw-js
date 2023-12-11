0fa1468d69f91a0bbbf26c1752d72a349f269739
ibdkn <ibudiakina@gmail.com>
2023-12-11 19:16:24 +0300

    completed 06.js

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -6,3 +6,9 @@ let passportWithAddress = {
         city: "LA"
     }
 };
+console.log(passportWithAddress.address.city);
+
+const cloneOfPassportWithAddress = structuredClone(passportWithAddress);
+cloneOfPassportWithAddress.address.city = 'Bobryisk';
+
+console.log(cloneOfPassportWithAddress.address.city);
